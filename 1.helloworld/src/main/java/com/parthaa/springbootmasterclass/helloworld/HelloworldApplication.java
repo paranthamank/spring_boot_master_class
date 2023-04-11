712c0bb7265f485f01c09f55978caddb69637188
paranthamank <69627733+paranthamank@users.noreply.github.com>
2023-04-11 22:06:19 +0530

    Lesson 6 - Ran Initial Spring Boot project and created a sample get api

--- 1.helloworld/src/main/java/com/parthaa/springbootmasterclass/helloworld/HelloworldApplication.java
+++ 1.helloworld/src/main/java/com/parthaa/springbootmasterclass/helloworld/HelloworldApplication.java
@@ -2,12 +2,39 @@ package com.parthaa.springbootmasterclass.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class HelloworldApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloworldApplication.class, args);
 	}
 
+	@GetMapping("/customer")
+	Customer getCustomer(){
+		return  new Customer(1L,"Parthaa");
+	}
+
+	class Customer{
+		private final Long id;
+		private final String name;
+
+		public Customer(Long id, String name) {
+			this.id = id;
+			this.name = name;
+		}
+
+		public Long getId() {
+			return id;
+		}
+
+		public String getName() {
+			return name;
+		}
+	}
+
 }
+
