7c4ad8862f5eb13aa23cd667229850a7e5e2ecba
paranthamank <69627733+paranthamank@users.noreply.github.com>
2023-04-11 22:47:01 +0530

    lesson 12 - Implemented first ever dependency injection

--- 1.helloworld/src/main/java/com/parthaa/springbootmasterclass/helloworld/customer/CustomerController.java
+++ 1.helloworld/src/main/java/com/parthaa/springbootmasterclass/helloworld/customer/CustomerController.java
@@ -1,13 +1,20 @@
 package com.parthaa.springbootmasterclass.helloworld.customer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 public class CustomerController {
+    @Autowired
+    private final CustomerService customerService;
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
 
     @GetMapping("/customer")
     Customer getCustomer(){
-        return  new Customer(1L,"Parthaa");
+        return  customerService.getCustomer();
     }
 }
--- 1.helloworld/src/main/java/com/parthaa/springbootmasterclass/helloworld/customer/CustomerService.java
+++ 1.helloworld/src/main/java/com/parthaa/springbootmasterclass/helloworld/customer/CustomerService.java
@@ -1,4 +1,13 @@
 package com.parthaa.springbootmasterclass.helloworld.customer;
 
+
+import org.springframework.stereotype.Component;
+
+@Component
 public class CustomerService {
+
+    Customer getCustomer(){
+        return  new Customer(1L,"Parthaa");
+    }
+
 }
