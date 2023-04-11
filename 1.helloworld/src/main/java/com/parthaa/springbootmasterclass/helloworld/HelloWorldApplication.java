c433ab808b11d9189c58ba16814125918483da79
paranthamank <69627733+paranthamank@users.noreply.github.com>
2023-04-11 22:09:38 +0530

    Changed Helloworld -> HelloWorld

--- 1.helloworld/src/main/java/com/parthaa/springbootmasterclass/helloworld/HelloWorldApplication.java
+++ 1.helloworld/src/main/java/com/parthaa/springbootmasterclass/helloworld/HelloWorldApplication.java
@@ -7,10 +7,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class HelloworldApplication {
+public class HelloWorldApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HelloworldApplication.class, args);
+		SpringApplication.run(HelloWorldApplication.class, args);
 	}
 
 	@GetMapping("/customer")
