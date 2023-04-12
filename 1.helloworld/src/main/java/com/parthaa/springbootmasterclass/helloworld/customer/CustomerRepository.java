e232eb72893a3d29291409cfc87fb91957688835
paranthamank <69627733+paranthamank@users.noreply.github.com>
2023-04-12 14:15:57 +0530

    lesson 15 - Changed superset @Component to respective annotations

--- 1.helloworld/src/main/java/com/parthaa/springbootmasterclass/helloworld/customer/CustomerRepository.java
+++ 1.helloworld/src/main/java/com/parthaa/springbootmasterclass/helloworld/customer/CustomerRepository.java
@@ -2,11 +2,12 @@ package com.parthaa.springbootmasterclass.helloworld.customer;
 
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import java.util.Collections;
 import java.util.List;
 
-@Component
+@Repository
 @Primary
 public class CustomerRepository implements CustomerRepo{
     @Override
